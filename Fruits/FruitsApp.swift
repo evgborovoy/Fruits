921b7398b94210e73ea3ddb2c69167e89dac966c
Evgeniy <borovoyevgen@gmail.com>
2024-11-29 01:53:41 +0400

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Evgeniy Borovoy on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
